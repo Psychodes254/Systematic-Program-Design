4a2a0b545c8675d192ebf8caed60b856ac42f5f5
Psychodes254 <justusmasika80@gmail.com>
2026-03-07 14:34:42 +0300

    Designed a Harry Potter family tree, worked on producing counterparts with their descendants using the self-reference design structure

---
+++ Week 6/6 b/hp-family-tree-starter.rkt
@@ -0,0 +1,131 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname hp-family-tree-starter) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+;; =================
+;; Data definitions:
+(define-struct potter (husband wife children))
+;; Potter is (make-potter String String ListOfChild)
+;; interp. potter is a family with husband wife and their children
+
+;; Child is one of:
+;; - String: Child with no further family
+;; - Potter: Child who has their own family
+;; interp. one child in the family tree
+
+;; ListOfChild is one of:
+;; - empty
+;; - (cons Child ListOfChild)
+;; interp. Children of Potter family
+
+(define D1 (make-potter "Bill Weasley" "Fleur Delacour"
+                        (list "Victoire Weasley" "Dominique Weasley" "Louis Weasley")))
+(define D2 (make-potter "Percy Weasley" "Audrey Weasley"
+                        (list "Molly Weasley" "Lucy Weasley")))
+(define D3 (make-potter "George Weasley" "Angelina Johnson"
+                        (list "Fred Weasley" "Roxanne Weasley")))
+(define D4 (make-potter "Ronald Weasley" "Hermione Granger"
+                        (list "Rose Weasley" "Hugo Granger-Weasley")))
+(define D5 (make-potter "James Potter" "Lily Luna Potter"
+                        (list "Sirius Potter" "Severus Potter" "Luna Potter")))
+(define ARTHUR (make-potter "Arthur Weasley" "Molly Prewett"
+                            (list "Charlie Weasley" "Fred Weasley" D1 D2 D3 D4 D5)))
+
+;; Templates
+#;
+(define (fn-for-child c)
+  (cond [(string? c) (...)]
+        [(potter? c) (fn-for-pot c)]))
+#;
+(define (fn-for-pot p)
+  (... (potter-husband p)
+       (potter-wife p)
+       (fn-for-loc (potter-children p))))
+#;
+(define (fn-for-loc loc)
+  (cond [(empty? loc) (...)]
+        [else
+         (... (fn-for-child (first loc))
+              (fn-for-loc (rest loc)))]))
+
+
+;; Child       -> ListOfString
+;; ListOfChild -> ListOfString
+;; return a two element list of patners if husband and wife else ListOfChild
+(check-expect (potter--loc empty) empty)
+(check-expect (potter--p D1) (list (list "Bill Weasley" "Fleur Delacour")
+                                   "Victoire Weasley" "Dominique Weasley" "Louis Weasley"))
+(check-expect (potter--p D2) (list (list "Percy Weasley" "Audrey Weasley")
+                                   "Molly Weasley" "Lucy Weasley"))
+(check-expect (potter--p D3) (list (list "George Weasley" "Angelina Johnson")
+                                         "Fred Weasley" "Roxanne Weasley"))
+(check-expect (potter--p D4) (list (list "Ronald Weasley" "Hermione Granger")
+                                   "Rose Weasley" "Hugo Granger-Weasley"))
+(check-expect (potter--p D5) (list (list "James Potter" "Lily Luna Potter")
+                                         "Sirius Potter" "Severus Potter" "Luna Potter"))
+(check-expect (potter--p ARTHUR) (list (list "Arthur Weasley" "Molly Prewett")
+                                       "Charlie Weasley" "Fred Weasley"
+                                       (list "Bill Weasley" "Fleur Delacour")
+                                       "Victoire Weasley" "Dominique Weasley" "Louis Weasley"
+                                       (list "Percy Weasley" "Audrey Weasley")
+                                       "Molly Weasley" "Lucy Weasley"
+                                       (list "George Weasley" "Angelina Johnson")
+                                       "Fred Weasley" "Roxanne Weasley"
+                                       (list "Ronald Weasley" "Hermione Granger")
+                                       "Rose Weasley" "Hugo Granger-Weasley"
+                                       (list "James Potter" "Lily Luna Potter")
+                                       "Sirius Potter" "Severus Potter" "Luna Potter")) 
+
+;(define (potter--child c) empty)          ;stubs
+;(define (potter--p p) empty)
+;(define (potter--loc loc) empty)
+
+(define (potter--child c)
+  (cond [(string? c) (cons c empty)]
+        [(potter? c) (potter--p c)]))
+
+(define (potter--p p)
+  (cons (list (potter-husband p)
+                (potter-wife p))
+       (potter--loc (potter-children p))))
+
+(define (potter--loc loc)
+  (cond [(empty? loc) empty]
+        [else
+         (append (potter--child (first loc))
+              (potter--loc (rest loc)))]))
+
+
+;; Child       -> String
+;; ListOfChild -> String
+;; produce name of all the family member in a given tree
+;(define (potter--lon empty) empty)
+(check-expect (potter--name D1) (list "Bill Weasley" "Fleur Delacour" "Victoire Weasley" "Dominique Weasley" "Louis Weasley"))
+(check-expect (potter--name D2) (list "Percy Weasley" "Audrey Weasley" "Molly Weasley" "Lucy Weasley"))
+(check-expect (potter--name D3) (list "George Weasley" "Angelina Johnson" "Fred Weasley" "Roxanne Weasley"))
+(check-expect (potter--name D4) (list "Ronald Weasley" "Hermione Granger" "Rose Weasley" "Hugo Granger-Weasley"))
+(check-expect (potter--name D5) (list "James Potter" "Lily Luna Potter" "Sirius Potter" "Severus Potter" "Luna Potter"))
+(check-expect (potter--name ARTHUR) (list "Arthur Weasley" "Molly Prewett" "Charlie Weasley" "Fred Weasley"
+               "Bill Weasley" "Fleur Delacour" "Victoire Weasley" "Dominique Weasley" "Louis Weasley"
+               "Percy Weasley" "Audrey Weasley" "Molly Weasley" "Lucy Weasley"
+               "George Weasley" "Angelina Johnson" "Fred Weasley" "Roxanne Weasley"
+               "Ronald Weasley" "Hermione Granger" "Rose Weasley" "Hugo Granger-Weasley"
+               "James Potter" "Lily Luna Potter" "Sirius Potter" "Severus Potter" "Luna Potter"))
+
+;(define (child--name c) "")     ;stubs
+;(define (potter--name n) "")
+;(define (potter--lon lon) empty)
+
+(define (child--name c)
+  (cond [(string? c) (cons c empty)]
+        [(potter? c) (potter--name c)]))
+
+(define (potter--name n)
+  (append (list (potter-husband n)
+                (potter-wife n))
+          (potter--lon (potter-children n))))
+
+(define (potter--lon lon)
+  (cond [(empty? lon) empty]
+        [else
+         (append (child--name (first lon))
+              (potter--lon (rest lon)))]))
