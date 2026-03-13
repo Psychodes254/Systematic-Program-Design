33c09704a7b91d5554eab8184eb38737c7adc4f6
Psychodes254 <justusmasika80@gmail.com>
2026-03-13 19:04:31 +0300

    Refactored the search function with Encapsulation

--- Week 6/6 b/find-person-starter.rkt
+++ Week 6/6 b/find-person-starter.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname find-person-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname find-person-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; Data definitions:
 
 (define-struct person (name age kids))
@@ -34,27 +34,27 @@
 
 ;; Element String -> Integer or false
 ;; search the entire tree with a given name, return age, else false
-(check-expect (check--lop empty "N2") false)
-(check-expect (check--person P1 "N1") 5)
-(check-expect (check--lop (cons P1 (cons P2 (cons P3 empty))) "N3" ) 15) 
-(check-expect (check--lop (cons P1 (cons P2 (cons P3 empty))) "N4") false)
-(check-expect (check--person P2 "N3") false)
-(check-expect (check--person P3 "N3") 15)
-(check-expect (check--person P4 "N6") false)
-(check-expect (check--person P2 "N4") false)
-(check-expect (check--person P4 "N1") 5)
+(check-expect (search-person P1 "N1") 5)
+(check-expect (search-person P2 "N3") false)
+(check-expect (search-person P3 "N3") 15)
+(check-expect (search-person P4 "N6") false)
+(check-expect (search-person P2 "N4") false)
+(check-expect (search-person P4 "N1") 5)
 
-;(define (check--person p s) 0)    ;stubs
+;(define (search-person p s) 0)    ;stubs
 ;(define (check--lop lop s) false)
 
-(define (check--person p s)
-  (if (string=? (person-name p) s)
-       (person-age p)  
-       (check--lop (person-kids p) s)))
+(define (search-person p s)
+  (local [(define (check--person p s)
+            (if (string=? (person-name p) s)
+                (person-age p)  
+                (check--lop (person-kids p) s)))
 
-(define (check--lop lop s)
-  (cond [(empty? lop) false]
-        [else
-         (if (not (false? (check--person (first lop) s)))
-             (check--person (first lop) s)
-              (check--lop (rest lop) s))]))
+          (define (check--lop lop s)
+            (cond [(empty? lop) false]
+                  [else
+                   (local [(define check (check--person (first lop) s))]
+                   (if (not (false? check))
+                       check
+                       (check--lop (rest lop) s)))]))]
+    (check--person p s)))
