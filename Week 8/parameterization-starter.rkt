a18c6992dee7863ca0cf4a74f5e7176980c5ddb5
Psychodes254 <justusmasika80@gmail.com>
2026-03-14 16:15:44 +0300

    Structured the functions to depwnd on itself using the check-expect, and getting the signature and purpose of the function

--- Week 8/parameterization-starter.rkt
+++ Week 8/parameterization-starter.rkt
@@ -1,36 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname parameterization-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; parameterization-starter.rkt
-
-;; Find area
-(define (area r)
-  (* pi (sqr r)))
-
-(area 4)
-(area 6)
-
-
 ;; ====================
-;; ListOfString -> Boolean
-;; produce true if los includes "UBC"
-(check-expect (contains-ubc? empty) false)
-(check-expect (contains-ubc? (cons "McGill" empty)) false)
-(check-expect (contains-ubc? (cons "UBC" empty)) true)
-(check-expect (contains-ubc? (cons "McGill" (cons "UBC" empty))) true)
+;; String (listof String) -> Boolean
+;; Produce true if los contains s
+(check-expect (contains? "McGill" empty) false)
+(check-expect (contains? "UBC" (cons "McGill" empty)) false)
+(check-expect (contains? "UBC" (cons "UBC" empty)) true)
+(check-expect (contains? "UBC" (cons "McGill" (cons "UBC" empty))) true)
+(check-expect (contains? "McGill" (cons "UBC" empty)) false)
+(check-expect (contains? "McGill" (cons "McGill" empty)) true)
+(check-expect (contains? "McGill" (cons "UBC" (cons "McGill" empty))) true)
 
-(define (contains-ubc? los) (contains? "UBC" los))
-
-;; ListOfString -> Boolean
-;; produce true if los includes "McGill"
-(check-expect (contains-mcgill? empty) false)
-(check-expect (contains-mcgill? (cons "UBC" empty)) false)
-(check-expect (contains-mcgill? (cons "McGill" empty)) true)
-(check-expect (contains-mcgill? (cons "UBC" (cons "McGill" empty))) true)
-
-(define (contains-mcgill? los) (contains? "McGill" los))
-
-;; General function to search for an item in a list
 (define (contains? s los)
   (cond [(empty? los) false]
         [else
@@ -40,21 +21,14 @@
 
 
 ;; ====================
-;; ListOfNumber -> ListOfNumber
-;; produce list of sqr of every number in lon
-(check-expect (squares empty) empty)
-(check-expect (squares (list 3 4)) (list 9 16))
+;; (X -> Y) (listof X)  -> (listof Y)
+;; given fn and (list n0 n1 ...) produce (list (fn n0) (fn n1) ...)
+(check-expect (map2 sqr empty) empty)
+(check-expect (map2 sqr (list 3 4)) (list 9 16))
+(check-expect (map2 sqrt (list 9 16)) (list 3 4))
+(check-expect (map2 abs (list -9 16 -6)) (list 9 16 6))
+(check-expect (map2 string-length (list "mash" "Kim" "John")) (list 4 3 4))
 
-(define (squares lon) (map2 sqr lon))
-
-;; ListOfNumber -> ListOfNumber
-;; produce list of sqrt of every number in lon
-(check-expect (square-roots empty) empty)
-(check-expect (square-roots (list 9 16)) (list 3 4))
-
-(define (square-roots lon) (map2 sqrt lon))
-
-;; Function to map the operation needed
 (define (map2 fn lon)
   (cond [(empty? lon) empty]
         [else
@@ -63,21 +37,13 @@
 
 
 ;; ====================
-;; ListOfNumber -> ListOfNumber
-;; produce list with only positive? elements of lon
-(check-expect (positive-only empty) empty)
-(check-expect (positive-only (list 1 -2 3 -4)) (list 1 3))
+;; (X -> Boolean) (listof X) -> (listof X)
+;; Given a list, produce a list of only the elements that satisfy the predicate p
+(check-expect (filter2 positive? empty) empty)
+(check-expect (filter2 negative? (list 1 -5 5 -1)) (list -5 -1))
+(check-expect (filter2 positive? (list 1 -5 5 -1)) (list 1 5))
+(check-expect (filter2 false? (list false true false false true)) (list false false false))
 
-(define (positive-only lon) (filter2 positive? lon))
-
-;; ListOfNumber -> ListOfNumber
-;; produce list with only negative? elements of lon
-(check-expect (negative-only empty) empty)
-(check-expect (negative-only (list 1 -2 3 -4)) (list -2 -4))
-
-(define (negative-only lon) (filter2 negative? lon))
-
-;; General function to check negative and positive numbers
 (define (filter2 fn lon)
   (cond [(empty? lon) empty]
         [else
